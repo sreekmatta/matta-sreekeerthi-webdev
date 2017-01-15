4d2a3e802b7b719db908fd13e899d54782a57187
sreematta <matta.s@husky.neu.edu>
2017-01-15 11:33:30 -0500

    Commit#ConnectionURL
    connection changed

--- test/app.js
+++ test/app.js
@@ -6,13 +6,14 @@ module.exports = function(app)
 
     var connectionString = 'mongodb://127.0.0.1:27017/test';
 
-    if(process.env.MLAB_USERNAME) {
-        connectionString = process.env.MLAB_USERNAME + ":" +
-            process.env.MLAB_PASSWORD + "@" +
-            process.env.MLAB_HOST + ':' +
-            process.env.MLAB_PORT + '/' +
-            process.env.MLAB_APP_NAME;
-    }
+    // if(process.env.MLAB_USERNAME) {
+    //     connectionString = process.env.MLAB_USERNAME + ":" +
+    //         process.env.MLAB_PASSWORD + "@" +
+    //         process.env.MLAB_HOST + ':' +
+    //         process.env.MLAB_PORT + '/' +
+    //         process.env.MLAB_APP_NAME;
+    // }
+        connectionString = "mongodb://admin:Bolsol@2016@ds111549.mlab.com:11549/heroku_4rnhbbns";
 
     var mongoose = require("mongoose");
     mongoose.connect(connectionString);
