1678fa35525772e2a79e8d53add15e0c1b3b7e90
sreematta <matta.s@husky.neu.edu>
2017-01-15 13:03:56 -0500

    Commit#URI String Commit
    URI environment variables

--- test/app.js
+++ test/app.js
@@ -6,14 +6,9 @@ module.exports = function(app)
 
     var connectionString = 'mongodb://127.0.0.1:27017/test';
 
-    // if(process.env.MLAB_USERNAME) {
-    //     connectionString = process.env.MLAB_USERNAME + ":" +
-    //         process.env.MLAB_PASSWORD + "@" +
-    //         process.env.MLAB_HOST + ':' +
-    //         process.env.MLAB_PORT + '/' +
-    //         process.env.MLAB_APP_NAME;
-    // }
-        connectionString = "mongodb://keerthi:abcd@ds111549.mlab.com:11549/heroku_4rnhbbns";
+    if(process.env.MLAB_USERNAME) {
+         connectionString = MONGODB_URI;
+    }
 
     var mongoose = require("mongoose");
     mongoose.connect(connectionString);
